258273c5bf62e46f2e571745deec92b0ffa2b646
crazydemo <452642781@qq.com>
2021-08-11 06:05:38 +0800

    enable bias_add, dense_add pattern

--- python/tvm/relay/op/contrib/dnnl.py
+++ python/tvm/relay/op/contrib/dnnl.py
@@ -63,7 +63,7 @@ _register_external_op_helper("nn.batch_norm")
 _register_external_op_helper("nn.conv2d")
 _register_external_op_helper("nn.dense")
 _register_external_op_helper("nn.relu")
-_register_external_op_helper("add")
+# _register_external_op_helper("add")
 _register_external_op_helper("subtract")
 _register_external_op_helper("multiply")
 
@@ -81,11 +81,23 @@ def make_pattern(with_bias=True, with_relu=True):
         return is_op("nn.relu")(conv_out)
     return conv_out
 
+def make_dense_pattern(with_bias=True):
+    data = wildcard()
+    weight = wildcard()
+    bias = wildcard()
+    dense = is_op("nn.dense")(data, weight)
+    if with_bias:
+        dense_out = is_op("add")(dense, bias)
+    else:
+        dense_out = dense
+    return dense_out
+
 
 @register_pattern_table("dnnl")
 def pattern_table():
     conv2d_bias_relu_pat = ("dnnl.conv2d_bias_relu", make_pattern(with_bias=True))
     conv2d_relu_pat = ("dnnl.conv2d_relu", make_pattern(with_bias=False))
-    # conv2d_bias_pat = ("dnnl.conv2d_bias", make_pattern(with_bias=True, with_relu=False))
-    dnnl_patterns = [conv2d_bias_relu_pat, conv2d_relu_pat]
+    conv2d_bias_pat = ("dnnl.conv2d_bias", make_pattern(with_bias=True, with_relu=False))
+    dense_bias_pat = ("dnnl.dense_bias", make_dense_pattern(with_bias=True))
+    dnnl_patterns = [conv2d_bias_relu_pat, conv2d_relu_pat, conv2d_bias_pat, dense_bias_pat]
     return dnnl_patterns
