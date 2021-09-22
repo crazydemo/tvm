d6c1e74b81551b986e656dfa63084782833deb6e
crazydemo <452642781@qq.com>
2021-09-22 18:17:53 +0800

    enable auto query in tir

--- tutorials/my_trials/test_latency_throughput.sh
+++ tutorials/my_trials/test_latency_throughput.sh
@@ -1,7 +1,7 @@
 OMP_NUM_THREADS=28 \
 KMP_AFFINITY=granularity=fine,noduplicates,compact,1,0 \
 numactl --physcpubind=0-27 --membind=0 \
-python $HOME/tvm/tutorials/my_trials/benchmark_byoc_dnnl.py --profiling=True
+python $HOME/tvm/tutorials/my_trials/benchmark_byoc_dnnl.py
 
 # OMP_NUM_THREADS=28 \
 # KMP_AFFINITY=granularity=fine,noduplicates,compact,1,0 \
