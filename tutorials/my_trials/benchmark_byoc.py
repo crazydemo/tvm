0aae4780e92666776c91248f7231381dfe794962
Ivy <yan3.zhang@intel.com>
2022-04-26 16:22:51 +0800

    set up squeezenet

--- tutorials/my_trials/benchmark_byoc.py
+++ tutorials/my_trials/benchmark_byoc.py
@@ -254,7 +254,7 @@ if __name__ == "__main__":
     parser.add_argument(
         "--network",
         type=str,
-        default="resnest50",
+        default="squeezenet1.0",
         help="The name of the neural network.",
     )
     parser.add_argument("--batch_size", type=int, default=1, help="The batch size")
